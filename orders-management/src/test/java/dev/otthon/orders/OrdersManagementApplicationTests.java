26a7c921a9c376f2c9a0ea1fa44cc04f2d1c99f9
Otthon Leão <otthonleao@hotmail.com>
2025-01-25 21:11:37 -0400

    initial project

---
+++ orders-management/src/test/java/dev/otthon/orders/OrdersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
