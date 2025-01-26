c29f256334b09a13e4dd1ee527f5f43141b67be3
Otthon Leão <otthonleao@hotmail.com>
2025-01-26 02:57:49 -0400

    feat: add Order endpoint and RabbitMQ

---
+++ orders-management/src/main/java/dev/otthon/orders/domain/OrderItem.java
@@ -0,0 +1,23 @@
+package dev.otthon.orders.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    private UUID id = UUID.randomUUID();
+
+    @JsonProperty("product")
+    private Product product;
+
+    @JsonProperty("quantity")
+    private Integer quantity;
+
+}
---
+++ orders-management/src/main/java/dev/otthon/orders/service/OrderService.java
@@ -0,0 +1,31 @@
+package dev.otthon.orders.service;
+
+import dev.otthon.orders.domain.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public Order enqueueOrder(Order order) {
+        rabbitTemplate.convertAndSend(exchangeName, "", order);
+        logger.info("Enfileirando pedido: {}", order);
+        return order;
+    }
+
+
+}
