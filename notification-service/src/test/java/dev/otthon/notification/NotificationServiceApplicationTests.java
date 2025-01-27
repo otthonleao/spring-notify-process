bddd8f68a2684147f8e4ed10a9af8c29e5e2a95b
Otthon Leão <otthonleao@hotmail.com>
2025-01-27 04:36:35 -0400

    first commit with dependencies

---
+++ notification-service/src/test/java/dev/otthon/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
