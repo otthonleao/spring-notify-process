737d4cf7b3b63426bc5361784213933ed8f17425
Otthon Leão <otthonleao@hotmail.com>
2025-01-27 22:39:13 -0400

    feat: initial project (order-processor) with dependencies

---
+++ order-processor-service/src/main/java/dev/otthon/processor/OrderProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProcessorServiceApplication.class, args);
+    }
+
+}
---
+++ order-processor-service/src/test/java/dev/otthon/processor/OrderProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
